c2dfd01b1be0cd5475e0206a8ad6eb972f4464ab
koiusa <71818379+koiusa@users.noreply.github.com>
2021-08-25 01:04:21 +0900

    UniVRM追加、UnityChanにSpringBone追加

---
+++ InteractiveRoom/Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
@@ -0,0 +1,11 @@
+
+namespace UniGLTF
+{
+    public static partial class UniGLTFVersion
+    {
+        public const int MAJOR = 2;
+        public const int MINOR = 17;
+        public const int PATCH = 0;
+        public const string VERSION = "2.17.0";
+    }
+}
