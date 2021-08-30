a543c74a94512519fcb68bf7b72e8c469d4f7ad9
koiusa <71818379+koiusa@users.noreply.github.com>
2021-08-31 04:10:41 +0900

    音データ成分抽出

--- InteractiveRoom/Assets/Koiusa/System/Sound/SpectrumAnalyzer.cs
+++ InteractiveRoom/Assets/Koiusa/System/Sound/SpectrumAnalyzer.cs
@@ -4,22 +4,26 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class SpectrumAnalyzer : MonoBehaviour
 {
-    private AudioSource audio_;
+    private const uint SampleCount = 1024;
+    private const float ClampBottom = -80.0f;
+    private const float FalldownPerTick = 0.1f;
+    private const float LevelRange = 5.0f;
 
+    private AudioSource _audio;
     private MusicUnity _musicUnity;
 
-    float[] spectrum = new float[1024];
+    float[] spectrum = new float[SampleCount];
     void Start()
     {
         _musicUnity = GetComponent<MusicUnity>();
-        audio_ = _musicUnity.transform.Find("audioSource_0").GetComponent<AudioSource>();//GetComponent<AudioSource>();
+        _audio = _musicUnity.transform.Find("audioSource_0").GetComponent<AudioSource>();
     }
 
     void Update()
     {
         if (_musicUnity.State == Music.PlayState.Playing)
         {
-            audio_.GetSpectrumData(spectrum, 0, FFTWindow.Hamming);
+            _audio.GetSpectrumData(spectrum, 0, FFTWindow.Hamming);
             for (int i = 1; i < spectrum.Length - 1; ++i)
             {
                 Debug.DrawLine(
@@ -38,7 +42,73 @@ public class SpectrumAnalyzer : MonoBehaviour
                         new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3),
                         new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3),
                         Color.yellow);
+
+                var hz = AudioSettings.outputSampleRate * 0.5f * i / SampleCount;
+
             }
+            var pitch = CalcPitch(spectrum);
+            var dbfs = CalcDecibel();
+            var peek = CalcPeakFallDownValue(dbfs);
         }
+
+    }
+
+    private float CalcDecibel()
+    {
+        float[] samples = new float[SampleCount];
+
+        _audio.GetOutputData(samples, 0);
+
+        var sum = 0.0f;
+        foreach (var sample in samples)
+        {
+            sum += sample * sample;
+        }
+
+        var rmsValue = Mathf.Sqrt(sum / SampleCount);
+        var dbValue = 20.0f * Mathf.Log10(rmsValue);
+        dbValue = Mathf.Clamp(dbValue, ClampBottom, float.MaxValue);
+        return dbValue;
+    }
+
+    private float CalcPitch(float[] samples)
+    {
+        var maxValue = 0.0f;
+        var maxIndex = 0;
+
+        for (var i = 0; i < SampleCount; i++)
+        {
+            var spectrum = samples[i];
+            if (maxValue > spectrum)
+                continue;
+
+            maxValue = spectrum;
+            maxIndex = i;
+        }
+
+        if (maxIndex > 0)
+        {
+            var l = samples[maxIndex - 1] / samples[maxIndex];
+            var r = samples[maxIndex + 1] / samples[maxIndex];
+            var f = maxIndex + 0.5f * (r * r - l * l);
+
+            return f * AudioSettings.outputSampleRate * 0.5f * maxIndex / SampleCount;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    private float CalcPeakFallDownValue(float db)
+    {
+        float peak = 0;
+        var delta = Time.deltaTime;
+
+        peak = Mathf.Max(peak - FalldownPerTick * delta, ClampBottom);
+        peak = Mathf.Clamp(db, peak, 0.0f);
+
+        var minValue = peak - LevelRange;
+        return (Mathf.Clamp(db, minValue, peak) - minValue) / LevelRange;
     }
 }
