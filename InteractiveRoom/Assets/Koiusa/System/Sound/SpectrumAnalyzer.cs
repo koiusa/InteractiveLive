4849498d8abfba1e62b3b4643053db85c5899e1a
koiusa <71818379+koiusa@users.noreply.github.com>
2021-09-01 01:26:31 +0900

    EnergyShieldシェーダー追加

--- InteractiveRoom/Assets/Koiusa/System/Sound/SpectrumAnalyzer.cs
+++ InteractiveRoom/Assets/Koiusa/System/Sound/SpectrumAnalyzer.cs
@@ -38,25 +38,25 @@ public class SpectrumAnalyzer : MonoBehaviour
         if (_musicUnity.State == Music.PlayState.Playing)
         {
             _audio.GetSpectrumData(spectrum, 0, FFTWindow.Hamming);
-            for (int i = 1; i < spectrum.Length - 1; ++i)
-            {
-                Debug.DrawLine(
-                        new Vector3(i - 1, spectrum[i] + 10, 0),
-                        new Vector3(i, spectrum[i + 1] + 10, 0),
-                        Color.red);
-                Debug.DrawLine(
-                        new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2),
-                        new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2),
-                        Color.cyan);
-                Debug.DrawLine(
-                        new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1),
-                        new Vector3(Mathf.Log(i), spectrum[i] - 10, 1),
-                        Color.green);
-                Debug.DrawLine(
-                        new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3),
-                        new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3),
-                        Color.yellow);
-            }
+            //for (int i = 1; i < spectrum.Length - 1; ++i)
+            //{
+            //    Debug.DrawLine(
+            //            new Vector3(i - 1, spectrum[i] + 10, 0),
+            //            new Vector3(i, spectrum[i + 1] + 10, 0),
+            //            Color.red);
+            //    Debug.DrawLine(
+            //            new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2),
+            //            new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2),
+            //            Color.cyan);
+            //    Debug.DrawLine(
+            //            new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1),
+            //            new Vector3(Mathf.Log(i), spectrum[i] - 10, 1),
+            //            Color.green);
+            //    Debug.DrawLine(
+            //            new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3),
+            //            new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3),
+            //            Color.yellow);
+            //}
             Hz = CalcHz(spectrum);
             DbfsPerHz = CalcDecibelPerHz();
             Pitch = CalcPitch(spectrum);
